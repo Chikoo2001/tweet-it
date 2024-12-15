cd27bb786cf7602561f571d7026a22374116c7f9
Chikoo2001 <chaitanyadharmgutte1234@gmail.com>
2024-12-15 14:17:34 +0530

    3- payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ v2.config({
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
