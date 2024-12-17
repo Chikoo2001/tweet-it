c9055b9b5d5be4f56bbba3dea30d44aee7e3d113
Chikoo2001 <chaitanyadharmgutte1234@gmail.com>
2024-12-18 00:27:13 +0530

    4- prepare app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -19,14 +20,24 @@ v2.config({
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json({limit: '5mb'}));
-app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
